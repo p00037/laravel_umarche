f21b89eeceffdb9268e3899e79890fe7f3691c0a
okabe <nagi0110@yahoo.co.jp>
2022-05-21 23:01:15 +0900

    index,create

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -17,20 +17,23 @@ class OwnersController extends Controller
 
     public function index()
     {
-        $data_now = Carbon::now();
-        $data_parse = Carbon::parse(now());
-        echo $data_now;
-        echo $data_parse;
+        // $data_now = Carbon::now();
+        // $data_parse = Carbon::parse(now());
+        // echo $data_now;
+        // echo $data_parse;
 
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
         // $q_first = DB::table('owners')->select('name')->first();
         // $c_test = collect([
         // 'name' => 'てすと'
         // ]);
 
         // dd($e_all, $q_get, $q_first, $c_test);
-        return view('admin.owners.index', compact('e_all', 'q_get'));
+
+
+        $owners = Owner::select('name','email','created_at')->get();
+        return view('admin.owners.index', compact('owners'));
     }
 
     /**
@@ -40,7 +43,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**
